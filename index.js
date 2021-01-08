8113c5bbbcd662c060caffdf8d2d2b8c2de33241
Seth Todd <seth.todd@outlook.com>
2021-01-07 20:48:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+let Breakfast = class {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    } 
+}
+
+let Lunch = class {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+let Dinner = class {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
